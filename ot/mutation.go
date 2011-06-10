0a8c00d9176467945b12794d26e2b27e9ad0dbc2
torben.weis@gmail.com <torben.weis@gmail.com@95bfe18c-3b8a-7236-569a-2b3c329eaaeb>
2011-06-10 13:19:04 +0000

    Docu
    git-svn-id: http://lightwave.googlecode.com/svn/trunk/experimental@129 95bfe18c-3b8a-7236-569a-2b3c329eaaeb

--- ot/mutation.go
+++ ot/mutation.go
@@ -17,17 +17,34 @@ const (
 
 type Operation struct {
   Kind int "k"
-  Len int "l"                  // Usually 1, except for SkipOp, DeleteOp (or InsertOp if used in a string mutation)
-  Operations []Operation "o" // Might be empty
-  Value interface{} "v"        // A simple value, e.g. string or int or float etc. Might be nil
+  // Usually 1, except for SkipOp, DeleteOp (or InsertOp if used in a string mutation)
+  Len int "l"
+  // The sub-operations. For example a StringOp will store in this field all the insert, skip and delete
+  // operations it wants to apply to a string.
+  // This field is empty for InsertOp, DeleteOp, SkipOp and OverwriteOp.
+  Operations []Operation "o"
+  // A simple value, e.g. string or int or float etc.
+  // This value is used in case of InsertOp and OverwriteOp.
+  // In case of InsertOp it stores the string value to insert.
+  // However, with InsertOp the Value might be an empty string while the Len field is larger than 0.
+  // This indicates that the operation wants to insert a number of tombs as specified by the Len field. 
+  Value interface{} "v"        
 }
 
 type Mutation struct {
-  Operation Operation "o" // The root of a tree of operations
-  ID string "id"            // A globally unique ID. This ID is used to break a tie if two operations are in conflict 
-  Site string "site"        // A site identifier. This an unstructured string which is unique for a combination of user and session.
-  Dependencies []string "d" // IDs of mutations on which this mutation depends on directly (indirect dependencies are not listed)
-  DebugName string "n"      // For debugging only
+  // The root of a tree of operations.
+  // This is eithr StringOp, ArrayOp, ObjectOp or OverwriteOp.
+  // Currently, only StringOp is supported.
+  Operation Operation "o" 
+  // A globally unique ID. This ID is used to break a tie if two operations are in conflict.
+  ID string "id" 
+  // A site identifier. This an unstructured string which is unique for a combination of user and session.
+  // This string is used to break a tie if two operations are in conflict.
+  Site string "site"
+  // IDs of mutations on which this mutation depends directly (indirect dependencies are not listed)
+  Dependencies []string "d"
+  // For debugging only
+  DebugName string "n"      
 }
 
 func (self Mutation) String() string {
