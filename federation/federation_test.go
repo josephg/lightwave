461c83e67732521bc7b9179ab027cf1a62e03b19
torben.weis@gmail.com <torben.weis@gmail.com@95bfe18c-3b8a-7236-569a-2b3c329eaaeb>
2011-06-30 12:38:30 +0000

    More testing. Code passes the tests
    git-svn-id: http://lightwave.googlecode.com/svn/trunk/experimental@186 95bfe18c-3b8a-7236-569a-2b3c329eaaeb

--- federation/federation_test.go
+++ federation/federation_test.go
@@ -32,6 +32,8 @@ func (self *dummyNameService) Lookup(identity string) (url string, err os.Error)
     return "http://localhost:8282/fed", nil
   case "c@charly":
     return "http://localhost:8383/fed", nil
+  case "d@daisy":
+    return "http://localhost:8484/fed", nil
   }
   return "", os.NewError("Unknown identity")
 }
@@ -48,21 +50,27 @@ func TestFederation(t *testing.T) {
   store1 := NewSimpleBlobStore()
   store2 := NewSimpleBlobStore()
   store3 := NewSimpleBlobStore()
+  store4 := NewSimpleBlobStore()
   fed1 := NewFederation("a@alice", "127.0.0.1:8181", ns, store1)
   fed2 := NewFederation("b@bob", "127.0.0.1:8282", ns, store2)
   fed3 := NewFederation("c@charly", "127.0.0.1:8383", ns, store3)
+  fed4 := NewFederation("d@daisy", "127.0.0.1:8484", ns, store4)
   indexer1 := idx.NewIndexer("a@alice", store1, fed1)
   indexer2 := idx.NewIndexer("b@bob", store2, fed2)
   indexer3 := idx.NewIndexer("c@charly", store3, fed3)
+  indexer4 := idx.NewIndexer("d@daisy", store4, fed4)
   app1 := &dummyAppIndexer{"a@alice", store1, fed1, t}
   indexer1.AddListener(app1)
   app2 := &dummyAppIndexer{"b@bob", store2, fed2, t}
   indexer2.AddListener(app2)
   app3 := &dummyAppIndexer{"c@charly", store3, fed3, t}
   indexer3.AddListener(app3)
+  app4 := &dummyAppIndexer{"d@daisy", store4, fed4, t}
+  indexer4.AddListener(app4)
   go runFed(t, fed1)
   go runFed(t, fed2)
   go runFed(t, fed3)
+  go runFed(t, fed4)
   
   time.Sleep(1000000000 * 2)
 
@@ -83,6 +91,8 @@ func TestFederation(t *testing.T) {
   blobref7 := NewBlobRef(blob7)
   blob8 := []byte(`{"type":"permission", "signer":"a@alice", "action":"invite", "perma":"` + blobref1 + `", "dep":["` + blobref4 + `"], "user":"c@charly", "allow":` + fmt.Sprintf("%v", idx.Perm_Read) + `, "deny":0}`)
   blobref8 := NewBlobRef(blob8)
+  blob9 := []byte(`{"type":"permission", "signer":"a@alice", "action":"invite", "perma":"` + blobref1 + `", "dep":["` + blobref4 + `"], "user":"d@daisy", "allow":` + fmt.Sprintf("%v", idx.Perm_Read) + `, "deny":0}`)
+  blobref9 := NewBlobRef(blob9)
 
   log.Printf("Hashes:\n1: %v\n1b: %v\n2: %v\n3: %v\n4: %v\n5: %v\n7: %v\n8: %v\n", blobref1, blobref1b, blobref2, blobref3, blobref4, blobref5, blobref7, blobref8)
   
@@ -98,7 +108,7 @@ func TestFederation(t *testing.T) {
   time.Sleep(1000000000 * 2)
 
   store1.StoreBlob(blob7, blobref7)
-//  store1.StoreBlob(blob8, blobref8)
+  store1.StoreBlob(blob9, blobref9)
 
   time.Sleep(1000000000 * 2)
   
@@ -106,21 +116,28 @@ func TestFederation(t *testing.T) {
   if err != nil {
     t.Fatal("Not a perma node")
   }
-  if len(p.Followers()) != 3 {
+  if len(p.Followers()) != 4 {
     t.Fatal("Indexer1 has wrong number of followers")
   }
   p, err = indexer2.PermaNode(blobref1)
   if err != nil {
     t.Fatal("Not a perma node")
   }
-  if len(p.Followers()) != 3 {
+  if len(p.Followers()) != 4 {
     t.Fatal("Indexer2 has wrong number of followers", p.Followers())
   }
   p, err = indexer3.PermaNode(blobref1)
   if err != nil {
     t.Fatal("Not a perma node")
   }
-  if len(p.Followers()) != 3 {
+  if len(p.Followers()) != 4 {
+    t.Fatal("Indexer3 has wrong number of followers", p.Followers())
+  }
+  p, err = indexer4.PermaNode(blobref1)
+  if err != nil {
+    t.Fatal("Not a perma node")
+  }
+  if len(p.Followers()) != 4 {
     t.Fatal("Indexer3 has wrong number of followers", p.Followers())
   }
 }
