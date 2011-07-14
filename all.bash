4b8dc11d7ed725c3e55ce0055c1f4f263f2da856
torben.weis@gmail.com <torben.weis@gmail.com@95bfe18c-3b8a-7236-569a-2b3c329eaaeb>
2011-07-14 23:09:52 +0000

    Updated build script
    git-svn-id: http://lightwave.googlecode.com/svn/trunk/experimental@193 95bfe18c-3b8a-7236-569a-2b3c329eaaeb

--- all.bash
+++ all.bash
@@ -4,3 +4,5 @@ cd store; make clean; make install; cd ..
 cd grapher; make clean; make install; cd ..
 cd federation; make clean; make install; cd ..
 cd transformer; make clean; make install; cd ..
+cd samples/gocurses; make clean; make install; cd ../..
+cd samples/p2p_editor; make clean; make; cd ../..
